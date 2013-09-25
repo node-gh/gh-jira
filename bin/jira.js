844ef6de3969f59c501d7ea938b3ab9a5b1ba4e7
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2013-09-25 17:05:14 -0300

    Comment on a ticket #4

--- bin/jira.js
+++ bin/jira.js
@@ -27,18 +27,18 @@ function Jira(options) {
 // -- Constants ----------------------------------------------------------------
 Jira.DETAILS = {
     alias: 'ji',
+    iterative: 'number',
     commands: [
-        'foo'
+        'comment'
     ],
     description: 'NodeGH plugin for integrating Jira, an issue management system.',
     options: {
-        'foo': Boolean
+        'comment': String,
+        'number': [String, Array]
     },
     shorthands: {
-        'f': [ '--foo' ]
-    },
-    payload: function(payload, options) {
-        options.foo = true;
+        'c': ['--comment'],
+        'n': ['--number']
     }
 };
 
@@ -53,11 +53,80 @@ Jira.prototype.run = function() {
     instance.api = new jira.JiraApi(
         config.jira.protocol, config.jira.host, config.jira.port,
         config.jira.user, config.jira.password, config.jira.api_version);
+
+    instance.registerLoggerHelpers_();
+
+    if (options.comment) {
+        logger.logTemplate(
+            '{{prefix}} [info] Adding comment on issue {{greenBright "#" options.number}}', {
+                options: options
+            });
+
+        instance.comment(function(err) {
+            logger.defaultCallback(
+                err, null, logger.compileTemplate('{{jiraIssueLink}}', {
+                    options: options
+                }));
+        });
     }
 };
 
-Boilerplate.prototype.foo = function() {
-    return 'NodeGH plugin boilerplate :)';
+Jira.prototype.comment = function(opt_callback) {
+    var instance = this,
+        config = base.getGlobalConfig(),
+        options = instance.options,
+        issue,
+        operations;
+
+    operations = [
+        function(callback) {
+            instance.getIssue_(options.number, function(err, data) {
+                if (!err) {
+                    issue = data;
+                }
+                callback(err);
+            });
+        },
+        function(callback) {
+            options.comment = logger.applyReplacements(options.comment) +
+                '{markdown}' + instance.expandEmojiToHtml_(config.signature) + '{markdown}';
+
+            instance.api.addComment(issue.id, options.comment, callback);
+        }
+    ];
+
+    async.series(operations, function(err) {
+        opt_callback && opt_callback(err);
+    });
+};
+
+Jira.prototype.expandEmojiToHtml_ = function(content) {
+    return content.replace(':octocat:', '![NodeGH](http://nodegh.io/images/octocat.png)');
+};
+
+Jira.prototype.getIssue_ = function(issueNumber, opt_callback) {
+    var instance = this;
+
+    instance.api.findIssue(issueNumber, function(err, issue) {
+        opt_callback && opt_callback(err, issue);
+    });
+};
+
+Jira.prototype.registerLoggerHelpers_ = function() {
+    var instance = this,
+        options = instance.options,
+        config = base.getGlobalConfig();
+
+    logger.registerHelper('jiraIssueLink', function() {
+        var link = url.format({
+            protocol: config.jira.protocol,
+            hostname: config.jira.host,
+            port: config.jira.port,
+            pathname: '/browse/' + options.number
+        });
+
+        return link;
+    });
 };
 
 exports.Impl = Jira;
